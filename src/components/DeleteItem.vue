335ad557793634495fd17857e0e6eb4f76bb6430
Albert Jain <jainalberto09@gmail.com>
2024-04-13 12:35:01 +0800

    DeleteItempage.vue Added this view for Routing

--- src/components/DeleteItem.vue
+++ src/components/DeleteItem.vue
@@ -1,4 +1,6 @@
 <script>
+
+
 </script>
 
 <template>
---
+++ src/router/DeleteItemPage.vue
@@ -0,0 +1,55 @@
+<!-- DeleteItemPage.vue -->
+<template>
+    <div>
+      <h2>Delete Product</h2>
+      <DeleteProduct :product="productToDelete" @delete="deleteProduct" @cancel="cancelDelete" />
+    </div>
+  </template>
+  
+  <script>
+  import DeleteProduct from '@/components/DeleteItem.vue'; 
+  
+  export default {
+    name: 'DeleteItemPage',
+    components: {
+      DeleteProduct,
+    },
+    data() {
+      return {
+        productToDelete: null,
+      };
+    },
+    methods: {
+
+      deleteProduct(product) {
+       
+        console.log('Deleting product:', product);
+      
+        this.$router.push('/product-list'); 
+      },
+
+      cancelDelete() {
+
+        console.log('Delete action canceled');
+      
+        this.$router.push('/product-list'); 
+      },
+    },
+    created() {
+   
+      const productId = this.$route.params.productId; 
+
+      this.productToDelete = {
+        id: productId,
+        name: 'Sample Product', 
+        description: 'Sample Description', 
+        price: 10.99, 
+      };
+    },
+  }
+  </script>
+  
+  <style>
+ 
+  </style>
+  
