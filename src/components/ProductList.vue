1ca07845595c4a16027d2166dca251178562e3a1
jaenarnel <jaenarnel@gmail.com>
2024-04-19 19:17:53 +0800

    EditProdut - Jaen

---
+++ src/components/ProductList.vue
@@ -0,0 +1,26 @@
+<!-- ProductList.vue -->
+<template>
+    <div>
+      <div v-for="product in products" :key="product.id">
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.description }}</p>
+        <p>{{ product.price }}</p>
+        <button @click="editProduct(product)">Edit</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['products'],
+    methods: {
+      editProduct(product) {
+        this.$emit('edit-product', product);
+      },
+      }
+    }
+  </script>
+  
+  <style scoped>
+  /* Add your scoped styles here */
+  </style>
